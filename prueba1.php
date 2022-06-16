ac3912524a4c61d98b44570d2d508142554ff347
Christian Maculus <cmaculus@focasoftware.com>
2022-06-16 17:19:28 -0300

    post

---
+++ prueba1.php
@@ -0,0 +1,39 @@
+<?php 
+include("./includes/connect.php");
+
+$fecha_desde="2022-06-01";
+$hora_hasta="00:00:00";
+
+$fecha_desde="2022-06-30";
+$hora_hasta="23:59:59";
+
+/*
+ Tenis, futbol 5 sintético, fútbol 7 en pasto, padel, cancha futbol baldosa
+
+
+canchas de tenis
+18
+19
+20
+21
+
+sitetico
+36
+
+padel 
+65
+
+balsosa
+66
+
+*/
+
+//echo json_encode(json_decode($json1), JSON_PRETTY_PRINT);
+
+$array_disponibilidad=array();
+$array_ocupacion=array();
+
+
+
+
+?>
